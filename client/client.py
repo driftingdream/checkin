06b3282a89cb8079f4cfd520c6fc65cb58c5d2d5
driftingdream <ezrage2001@outlook.com>
2019-12-24 10:45:46 +0800

    added few codes in client.py

---
+++ client/client.py
@@ -0,0 +1,16 @@
+import config
+import socket
+import tkinter as tk
+
+class Application(tk.Frame):
+    def __init__(self, master):
+        self.master = master
+        super().__init__(master)
+        self.pack()
+        self.creat_widgets()
+    def creat_widgets():
+        
+def main():
+
+if __name__ == '__main__':
+    main()
