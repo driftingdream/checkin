bfb34a3ae2bf1568b0136e596b9431f953b7b088
driftingdream <ezrage2001@outlook.com>
2019-12-24 10:35:07 +0800

    modified the file structure

--- client/config.py
+++ client/config.py
@@ -2,4 +2,5 @@ import os, platform
 
 IP = '127.0.0.1'
 PORT = 6666
+IP_PORT = (IP, PORT)
 TIMEOUT= (10,60)
---
+++ server/server.py
@@ -0,0 +1,18 @@
+import socket
+
+
+STATUS = 0
+
+
+def main():
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
